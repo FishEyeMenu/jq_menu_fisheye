c38400610c81f7f1436a5e6bf4f532b7f6354a98
diegovernan <32338336+diegovernan@users.noreply.github.com>
2018-03-14 17:09:34 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Menu fisheye</title>
+	<style type="text/css">
+	.menu {
+		width: 1000px;
+		height: 70px;
+		background-color: #DDD;
+		position: fixed;
+		bottom: 0;
+		left: 50%;
+		margin-left: -500px;
+	}
+
+	.icone {
+		width: 70px;
+		height: 70px;
+		background-color: #AAA;
+		float: left;
+		margin-right: 5px;
+		margin-left: 5px;
+		cursor: pointer;
+	}
+</style>
+</head>
+<body>
+
+	<div class="menu">
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+		<div class="icone"></div>
+	</div>
+
+	<script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
